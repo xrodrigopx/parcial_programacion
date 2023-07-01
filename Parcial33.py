fc3edcc4ef0f74009d1582031f87f4ae21ebb65a
Rodrigo Perdomo <97471272+xrodrigopx@users.noreply.github.com>
2023-07-01 12:21:22 -0300

    hh

--- Parcial33.py
+++ Parcial33.py
@@ -1,34 +1,84 @@
-def clase_ip(dir_ip):
-    try:
-    
-        if decimal == 0:
-            return "0"
-        
-        binario = ""
-        
-        while decimal > 0:
-            bit = decimal % 2
-            binario = str(bit) + binario
-            decimal = decimal // 2
-            
-   
-        # Determinar la clase basada en el primer octeto
-        primer_octeto = binario[0]
-        if primer_octeto.startswith('0'):
-            return 'A'
-        elif primer_octeto.startswith('10'):
-            return 'B'
-        elif primer_octeto.startswith('110'):
-            return 'C'
-        elif primer_octeto.startswith('1110'):
-            return 'D'
-        elif primer_octeto.startswith('1111'):
-            return 'E'
-        else:
-            return 'No válida'
-    except ValueError:
-        return 'No válida'
+class Hora():
+    def __init__(self):
+        self.h = 0
+        self.m = 0
+        self.s = 0
 
-dir_ip = "192.168.2.1"
-clase = clase_ip(dir_ip)
-print("La ip: " + dir_ip + " es de clase: " + clase)
+    def getH(self):
+        return self.h
+
+    def setH(self, h):
+        self.h =h
+
+    def getM(self):
+        return self.m
+
+    def setM(self, m):
+        self.m = m
+
+    def getS(self):
+        return self.s
+
+    def setS(self, s):
+        self.s = s
+
+    def validar(self):
+        hora_valida = True
+        if ( self.h < 0 or self.h > 23):
+            hora_valida = False
+
+        if ( self.m < 0 or self.m > 59):
+            hora_valida = False
+
+        if ( self.s < 0 or self.s > 59):
+            hora_valida = False
+
+        if not hora_valida:
+            self.h = 0
+            self.m = 0
+            self.s = 0
+
+    def aSegundos(self):
+        return self.h * 60 * 60 + self.m * 60 + self.s
+
+    def deSegundos(self, segundos):
+        self.h = segundos // 3600
+        self.m = ( segundos % 3600 ) // 60
+        self.s = segundos - self.h * 3600 - self.m * 60
+
+
+    def segundosDesde(self, hora):
+        return abs( self.aSegundos() - hora.aSegundos() )
+
+    def siguiente(self):
+        # Paso a segundos y sumo 1, si el resultados ed 86400
+        # la hora pasa a ser 00:00:00
+        segundos = self.aSegundos()
+        segundos += 1
+        if segundos >= 86400:
+            segundos = 0
+        self.deSegundos(segundos)
+
+    def anterior(self):
+        # Paso a segundos y resto 1
+        # en el caso que segundos sea 0, la hora
+        # pasara a ser 23:59:59
+        segundos = self.aSegundos()
+        segundos -= 1
+        if segundos <= 0:
+            segundos = 86399
+        self.deSegundos(segundos)
+
+    def copia(self):
+        h = Hora()
+        h.setH(self.h)
+        h.setM(self.m)
+        h.setS(self.s)
+        return h
+
+    def __repr__(self):
+        return "{:02d}hs. {:02d}ms. {:02d}s.".format(
+            self.h,
+            self.m,
+            self.s
+        )
